4f6c72f966f6bfe2d326bc58a9b72b521c39aa19
gregwhitaker <gwhitake@gmail.com>
2015-05-23 16:10:09 -0700

    Rearranged to they are like a traffic light... OCD

--- utils/logging.sh
+++ utils/logging.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-# Logs the supplied parameter in ANSI green
-logGreen() {
-	echo -e "$(tput setaf 2)$1$(tput sgr0)"
-}
-
 # Logs the supplied parameter in ANSI red
 logRed() {
 	echo -e "$(tput setaf 1)$1$(tput sgr0)"
@@ -13,6 +8,11 @@ logRed() {
 # Logs the supplied parameter in ANSI yellow
 logYellow() {
 	echo -e "$(tput setaf 3)$1$(tput sgr0)"
+}
+
+# Logs the supplied parameter in ANSI green
+logGreen() {
+	echo -e "$(tput setaf 2)$1$(tput sgr0)"
 }
 
 # Logs the supplied parameter in ANSI white
