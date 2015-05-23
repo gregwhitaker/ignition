20dc1e4a11632262596a491b49a8f954403f5450
gregwhitaker <gwhitake@gmail.com>
2015-05-23 16:09:13 -0700

    Added comments

--- utils/logging.sh
+++ utils/logging.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
+# Logs the supplied parameter in ANSI green
 logGreen() {
 	echo -e "$(tput setaf 2)$1$(tput sgr0)"
 }
 
+# Logs the supplied parameter in ANSI red
 logRed() {
 	echo -e "$(tput setaf 1)$1$(tput sgr0)"
 }
 
+# Logs the supplied parameter in ANSI yellow
 logYellow() {
 	echo -e "$(tput setaf 3)$1$(tput sgr0)"
 }
 
+# Logs the supplied parameter in ANSI white
 log() {
 	echo $1
 }
