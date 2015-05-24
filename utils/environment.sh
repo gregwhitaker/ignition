6a00f9fb29edcaa4ded13ee8426e2cf73a257e1e
gregwhitaker <gwhitake@gmail.com>
2015-05-24 11:39:32 -0700

    Added documentation

--- utils/environment.sh
+++ utils/environment.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+#/ This script checks to see if the AWS CLI "REGION" environment variable has been set.  If not, this
+#/ script sets the environment variable to the default region specified in the users AWS credentials file.
+
 DEFAULT_REGION=$(aws configure get region)
 
 if [[ -z "$REGION" ]]; then
