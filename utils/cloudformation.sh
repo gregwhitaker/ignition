97ef4739fa3f24eee3bcd5e17b3f01f54f13021d
gregwhitaker <gwhitake@gmail.com>
2015-05-23 17:18:12 -0700

    Added cloudformation utils

---
+++ utils/cloudformation.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+waitForStack() {
+	echo -n "Waiting for stack to complete"
+    TEST_CONTENT=$(aws cloudformation describe-stacks --stack-name $1 --region $REGION | grep \"StackStatus\")
+
+    CREATE_COMPLETE=$(echo $TEST_CONTENT | grep 'CREATE_COMPLETE' | wc -l)
+    while [[ $CREATE_COMPLETE -lt 1 && $UPDATE_COMPLETE -lt 1 && $ROLLBACK -lt 1 ]]; do
+        sleep 2
+        echo -n "."
+
+        TEST_CONTENT=$(aws cloudformation describe-stacks --stack-name $1 --region $REGION | grep \"StackStatus\")
+        CREATE_COMPLETE=$(echo $TEST_CONTENT | grep 'CREATE_COMPLETE' | wc -l)
+        UPDATE_COMPLETE=$(echo $TEST_CONTENT | grep 'UPDATE_COMPLETE' | wc -l)
+        ROLLBACK=$(echo $TEST_CONTENT | grep 'ROLLBACK_COMPLETE' | wc -l)
+    done
+
+    echo ""
+
+    RETURN_VAL=0
+    if [ "$ROLLBACK" == "1" ]; then
+        RETURN_VAL=1
+    fi
+
+    return $RETURN_VAL
+}
