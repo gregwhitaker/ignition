fed263511c419ab967f497e86e2fc80c9e8f3819
gregwhitaker <gwhitake@gmail.com>
2015-05-24 11:34:14 -0700

    Renaming

--- utils/s3.sh
+++ utils/s3.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
-function find_bucket() {
+#/ Usage: find_bucket <bucket>
+#/ Finds an S3 Bucket by name.
+find_bucket() {
   BUCKET_NAME=$1
   OUTPUT=$(aws s3 ls s3://$BUCKET_NAME 2>&1)
   return $?
 }
 
-function flush_bucket() {
+#/ Usage: delete_bucket <bucket>
+#/ Deletes an S3 Bucket.
+delete_bucket() {
   BUCKET_NAME=$1
   echo “Flushing bucket $BUCKET_NAME"
   find_bucket $BUCKET_NAME
   if [ $? -ne 0 ]; then
     echo "Bucket not found"
   else
-    aws s3 rm "s3://$BUCKET_NAME" --region $REGION --recursive
+    delete_bucket $BUCKET_NAME
   fi
 }
+
