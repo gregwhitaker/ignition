91ac8ed502e71d1f6f49df0725f37ae339a23337
gregwhitaker <gwhitake@gmail.com>
2015-05-24 11:37:27 -0700

    Added documentation

--- utils/logging.sh
+++ utils/logging.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 
-# Logs the supplied parameter in ANSI red
+#/ Usage: log_red <message>
+#/ Logs the supplied parameter in ANSI red.
 log_red() {
 	echo -e "$(tput setaf 1)$1$(tput sgr0)"
 }
 
-# Logs the supplied parameter in ANSI yellow
+#/ Usage: log_yellow <message>
+#/ Logs the supplied parameter in ANSI yellow
 log_yellow() {
 	echo -e "$(tput setaf 3)$1$(tput sgr0)"
 }
 
-# Logs the supplied parameter in ANSI green
+#/ Usage: log_green <message>
+#/ Logs the supplied parameter in ANSI green
 log_green() {
 	echo -e "$(tput setaf 2)$1$(tput sgr0)"
 }
 
-# Logs the supplied parameter in ANSI white
+#/ Usage: log <message>
+#/ Logs the supplied parameter in ANSI white
 log() {
 	echo $1
 }
