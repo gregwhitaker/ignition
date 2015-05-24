7dba52d51ea5701b82ea2e32a075be0789a020c0
gregwhitaker <gwhitake@gmail.com>
2015-05-24 12:34:19 -0700

    Added required parameter check

--- cloudformation/setup-nested-stacks.sh
+++ cloudformation/setup-nested-stacks.sh
@@ -4,5 +4,15 @@ source ../utils/environment.sh
 source ../utils/logging.sh
 source ../utils/cloudformation.sh
 
+usage() {
+	echo "Usage: setup-nested-stacks <bucket>"
+}
+
+if [ -z "$1" ]; then
+	echo "S3 bucket name is required!"
+	usage
+	exit 1
+fi
+
 aws cloudformation create-stack --stack-name nested-stacks-s3 --template-body file://./templates/nested-stacks-s3.json --region $REGION --parameters ParameterKey=S3BucketName,ParameterValue=$1
 wait_for_stack "nested-stacks-s3"
