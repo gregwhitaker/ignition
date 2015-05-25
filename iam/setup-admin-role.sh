31a1d6e82a9477d347e482769b66ef1b4d267199
gregwhitaker <gwhitake@gmail.com>
2015-05-25 16:09:43 -0700

    Added scripts to setup roles

--- iam/setup-admin-role.sh
+++ iam/setup-admin-role.sh
@@ -7,5 +7,6 @@ source ../utils/environment.sh
 source ../utils/logging.sh
 source ../utils/cloudformation.sh
 
+# Creating Role
 aws cloudformation create-stack --stack-name "iam-admin-role" --template-body file://./templates/iam-admin-role.json --region $REGION
 wait_for_stack "iam-admin-role"
--- iam/setup-readonly-role.sh
+++ iam/setup-readonly-role.sh
@@ -7,5 +7,6 @@ source ../utils/environment.sh
 source ../utils/logging.sh
 source ../utils/cloudformation.sh
 
+# Creating Role
 aws cloudformation create-stack --stack-name "iam-readonly-role" --template-body file://./templates/iam-readonly-role.json --region $REGION
 wait_for_stack "iam-readonly-role"
