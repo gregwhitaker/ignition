af4b4586f690b8f98224601e0f918a2798c8475e
gregwhitaker <gwhitake@gmail.com>
2015-05-24 19:46:10 -0700

    Added parameter check to alias scripts

--- iam/setup-account-alias.sh
+++ iam/setup-account-alias.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
+#/ Usage: setup-account-alias <alias>
+#/ Creates an alias for the AWS account.
+
 source ../utils/logging.sh
 
-log "Creating Account Alias: $1"
+usage() {
+	echo "Usage: setup-account-alias <alias>"
+}
+
+if [ -z "$1" ]; then
+	echo "Alias is required!"
+	usage
+	exit 1
+fi
 
 aws iam create-account-alias --account-alias $1
--- iam/teardown-account-alias.sh
+++ iam/teardown-account-alias.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
+#/ Usage: teardown-account-alias <alias>
+#/ Creates an alias for the AWS account.
+
 source ../utils/logging.sh
 
-log "Deleting Account Alias: $1"
+usage() {
+	echo "Usage: teardown-account-alias <alias>"
+}
+
+if [ -z "$1" ]; then
+	echo "Alias is required!"
+	usage
+	exit 1
+fi
 
 aws iam delete-account-alias --account-alias $1
