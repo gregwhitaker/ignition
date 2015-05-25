fdd86000b0f813ca80ec61f63070587465d76e97
gregwhitaker <gwhitake@gmail.com>
2015-05-24 20:17:25 -0700

    Making the script fancier

--- ignition.sh
+++ ignition.sh
@@ -3,4 +3,25 @@
 source ./utils/environment.sh
 source ./utils/logging.sh
 
-python ./setup/installer.py
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -i|--install)
+    INSTALL="YES"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ "$INSTALL" = "YES" ]; then
+	log_green "Installing Ignition..."
+	sudo pip install -r ./setup/requirements.txt
+	log_green "Installation Complete!"
+else
+	python ./setup/installer.py
+fi
