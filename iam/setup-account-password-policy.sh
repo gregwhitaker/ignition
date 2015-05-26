5d9232c10c9cda1684859cd7ea7a9b5035f90f24
gregwhitaker <gwhitake@gmail.com>
2015-05-25 20:44:37 -0700

    Added script to configure the default IAM password policy

---
+++ iam/setup-account-password-policy.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#/ Usage: setup-account-password-policy
+#/ Sets the accounts password policy to the following:
+#/   - Minimum Password Length: 8
+#/	 - Require Symbols: Yes
+#/   - Require Numbers: Yes
+#/   - Require Uppercase: Yes
+#/   - Require Lowercase: Yes
+#/   - Maximum Age: No Expiry
+#/   - Allow Users to Change Password: Yes
+#/   - Password Reuse Prevention: 6 Most Recent Passwords
+
+source ../utils/logging.sh
+
+aws iam update-account-password-policy \
+	--minimum-password-length 8 \
+	--require-symbols \
+	--require-numbers \
+	--require-uppercase-characters \
+	--require-lowercase-characters \
+	--allow-users-to-change-password \
+	--password-reuse-prevention 6
+
+log_green "New Password Policy Configured!"
