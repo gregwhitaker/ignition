70a60b9653598d8b7c1ee33c15a986c9a2d720dc
gregwhitaker <gwhitake@gmail.com>
2015-05-31 17:12:44 -0700

    Implemented nested stacks

--- cloudformation/nested-stacks.py
+++ cloudformation/nested-stacks.py
@@ -3,30 +3,30 @@ from colorama import Fore, init
 
 class NestedStacks:
     """
-
+    Creates an S3 bucket to store CloudFormation Nested Stacks (reusable templates)
     """
 
     def __init__(self, bucket):
         """
+        Initializes this instance of NestedStacks
 
-        :param bucket:
-        :return:
+        :param bucket: S3 bucket name
         """
         self.bucket = bucket
 
     def setup(self):
         """
-
-        :return:
+        Creates an S3 bucket to store CloudFormation Nested Stacks
         """
-        print "test"
+        utils.s3.create_bucket(self.bucket)
 
     def teardown(self):
         """
 
         :return:
         """
-        print "test"
+        utils.s3.delete_bucket(self.bucket)
+
 
 if __name__ == '__main__':
     if __package__ is None:
@@ -34,12 +34,14 @@ if __name__ == '__main__':
         from os import path
         sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
         from utils.logging import log_error
+        import utils.s3
 
         # Initializing colorama because this method is currently being called
         # as a top-level script and not a module.
         init()
     else:
         from utils.logging import log_error
+        import utils.s3
 
     parser = argparse.ArgumentParser(description="Adds or removes S3 bucket to hold Cloudformation Nested Stacks.")
 
@@ -47,11 +49,11 @@ if __name__ == '__main__':
     command_group.add_argument("-s", "--setup", help="Adds the S3 bucket to the account.", action="store_true")
     command_group.add_argument("-t", "--teardown", help="Removes the S3 bucket from the account.", action="store_true")
 
-    parser.add_argument("--bucket", required=True, action="store", dest="alias", help="S3 bucket name")
+    parser.add_argument("--bucket", required=True, action="store", dest="bucket", help="S3 bucket name")
 
     args = parser.parse_args()
 
-    nested_stacks = NestedStacks(args.alias)
+    nested_stacks = NestedStacks(args.bucket)
 
     if args.setup:
         nested_stacks.setup()
--- utils/s3.py
+++ utils/s3.py
@@ -2,7 +2,7 @@ import boto.s3
 import boto.s3.connection
 import boto.s3.acl
 
-def is_bucket(name, region):
+def is_bucket(name):
     """
     Checks to see if an S3 bucket exists with a given name.
 
@@ -10,14 +10,14 @@ def is_bucket(name, region):
     :param name: bucket name
     :return: True if the bucket exists; otherwise False
     """
-    conn = boto.s3.connect_to_region(region)
+    conn = boto.s3.connection.S3Connection()
     if not conn.lookup(name) is None:
         return True
     else:
         return False
 
 
-def create_bucket(name, region, **kwargs):
+def create_bucket(name, **kwargs):
     """
     Creates a new S3 bucket.
 
@@ -25,11 +25,11 @@ def create_bucket(name, region, **kwargs):
     :param region:
     :return:
     """
-    conn = boto.s3.connect_to_region(region)
+    conn = boto.s3.connection.S3Connection()
     conn.create_bucket(name)
 
 
-def delete_bucket(name, region, **kwargs):
+def delete_bucket(name, **kwargs):
     """
     Deletes an existing S3 bucket.
 
@@ -37,22 +37,27 @@ def delete_bucket(name, region, **kwargs):
     :return:
     """
     if is_bucket(name):
-        if not kwargs['flush'] is None and kwargs['flush'] is True:
-            flush_bucket(name, region)
+        conn = boto.s3.connection.S3Connection()
 
-        conn = boto.s3.connect_to_region(region)
+        if 'flush' in kwargs:
+            if kwargs['flush'] is True:
+                flush_bucket(name)
+        else:
+            # Flusing bucket first by default
+            flush_bucket(name)
+
         conn.delete_bucket(name)
 
 
-def flush_bucket(name, region):
+def flush_bucket(name):
     """
     Flushes an existing S3 bucket.
 
     :param name:
     :return:
     """
-    if is_bucket(name, region):
-        conn = boto.s3.connect_to_region(region)
+    if is_bucket(name):
+        conn = boto.s3.connection.S3Connection()
         bucket = boto.s3.connection.Bucket(conn, name)
 
         for folder in bucket.list():
